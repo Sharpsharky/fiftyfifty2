ca6580e8d0e53477e0a5a997734410a9e10dbe0f
Sharpshark <maciej.jasielewicz@gmail.com>
2019-09-15 11:48:00 +0200

    Add Main Menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using Inputs;
+
+public class PauseMenu : MonoBehaviour, IButtonListener
+{
+    private bool isPaused = false; 
+    private void Update() {
+        
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+            Debug.Log("Escape pressed!");
+            isPaused = !isPaused;
+        } 
+
+        this.gameObject.transform.GetChild(0).gameObject.SetActive(isPaused);
+        Time.timeScale = (isPaused ? 0 : 1);
+    }
+
+
+    public void Quit() {
+        SceneManager.LoadSceneAsync(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void OnButtonPressed(ButtonCode code)
+    {
+        if (code == ButtonCode.Start)
+        {
+            isPaused = !isPaused;   
+        }
+    }
+
+    public void OnButtonReleased(ButtonCode code)
+    {
+    }
+
+    public void OnButtonHeld(ButtonCode code)
+    {
+    }
+}
--- Assets/SettingsMenu.cs
+++ Assets/SettingsMenu.cs
@@ -2,16 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Audio;
+using UnityEngine.UI;
+
 
 public class SettingsMenu : MonoBehaviour
 {
 
     public AudioMixer audioMixer;
+    public Toggle audioToggle;
 
     public void ToggleAudio(bool isChecked){
 
         int newVolume = -80;
-        if (isChecked) {
+        if (!isChecked) {
             newVolume = 0;
         }
 
@@ -20,6 +23,14 @@ public class SettingsMenu : MonoBehaviour
 
     public void SetQuality(int index) {
         QualitySettings.SetQualityLevel(index);
+    }
+
+    void update()
+    {
+        float volume = 0f;
+        audioMixer.GetFloat("volume", out volume);
+        bool isOn = (volume < 0 ? true : false);
+        audioToggle.isOn = isOn;
     }
 
 }
