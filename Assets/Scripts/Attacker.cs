1c28d7ce15d7893a262450e00b184d2b0d64cae3
Sharpshark <maciej.jasielewicz@gmail.com>
2019-09-15 08:49:43 +0200

    Life

---
+++ Assets/Scripts/Attacker.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attacker : MonoBehaviour
+{
+
+    public int attackPoints = 1;
+
+    private bool afterSpawnTime = false;
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if (other.gameObject.tag == "Player") {
+            if (other.gameObject.GetComponent<Life>() != null) {
+                other.gameObject.GetComponent<Life>().TakeDamage(attackPoints);
+                Debug.Log("Hit for:" + attackPoints);
+            } else {
+                Debug.LogError("Could not find 'Life' script on the player!");
+            }
+        }
+    }
+
+    private void Start() {
+        StartCoroutine(WaitForAppearance());
+    }
+    private void Update() {
+        
+        if (afterSpawnTime) {
+            Vector3 maxPos = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width,0,0));
+            Debug.Log(gameObject.transform.position.x + " ___ " + maxPos.x);
+            if (gameObject.transform.position.x > maxPos.x + 10 || gameObject.transform.position.x < 0 - 10) {
+                Destroy(gameObject);
+            }
+        }
+    }
+
+    IEnumerator WaitForAppearance() 
+    {
+        yield return new WaitForSeconds(2);
+        afterSpawnTime = true;
+    }
+}
---
+++ Assets/Scripts/Life.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+using Inputs;
+public class Life : MonoBehaviour
+{
+    public List<GameObject> hearts = new List<GameObject>();
+    public int maxHP = 3;
+    private int hp;
+
+    PlayerMove pm;
+    PlayerGrab pg;
+    PInput pi;
+
+    private void Start() {
+        pm = GetComponent<PlayerMove>();
+        pg = GetComponent<PlayerGrab>();
+        pi = GetComponent<PInput>();
+
+        hp = maxHP;
+    }
+    public void TakeDamage(int dmg) 
+    {
+        Debug.Log("Current HP "+ hp + " - dmg " + dmg + " = " + (hp-dmg));
+        if ((hp - dmg) > 0) {
+            var tempColor = hearts[hp - 1].GetComponent<Image>().color;
+            tempColor.a = 0;
+            hearts[hp - 1].GetComponent<Image>().color = tempColor;
+            hp -= dmg;
+        } else {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        Debug.Log("He died!");
+        StartCoroutine(SceneReload());
+    }
+
+    IEnumerator SceneReload()
+    {
+        pm.enabled = false;
+        pg.enabled = false;
+        pi.enabled = false;
+
+
+        yield return new WaitForSeconds(1f);
+
+
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+
+        yield return null;
+    }
+
+
+}
